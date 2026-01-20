a971734fe4ce2ac7e28808f11ddf334400b7160b
abdessalem <mohellebiabdessalem@gmail.com>
2026-01-20 12:26:19 +0100

    add material_mavigation example

---
+++ material_navigation/android/app/src/main/kotlin/com/abdesalem/mohellebi/material_navigation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abdesalem.mohellebi.material_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ material_navigation/lib/main.dart
@@ -0,0 +1,61 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NavigationExampleApp());
+}
+
+class NavigationExampleApp extends StatelessWidget {
+  const NavigationExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // The MaterialApp's home is automatically set as the bottom of the navigation stack
+    return MaterialApp(
+      title: 'Navigation Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Screen')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go to Second'),
+          onPressed: () {
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (_) => SecondScreen()));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go to First'),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+    );
+  }
+}
