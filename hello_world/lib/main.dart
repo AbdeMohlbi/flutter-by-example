0fed48da428d295bf53b7ce40b06e7eb652cda36
abdessalem <mohellebiabdessalem@gmail.com>
2026-01-20 12:20:50 +0100

    update Naming to match the example's name

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const HelloWorldExample());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HelloWorldExample extends StatelessWidget {
+  const HelloWorldExample({super.key});
 
   @override
   Widget build(BuildContext context) {
